de787b1919033e9e38f9253c95dc280ee457625e
Nikhil <patangenikhil08@gmail.com>
2021-06-05 20:23:32 +0530

    integrated with stripe payments , changed title and favicon

--- src/components/collections-overview/collections-overview.component.js
+++ src/components/collections-overview/collections-overview.component.js
@@ -4,7 +4,7 @@ import { createStructuredSelector} from 'reselect';
 
 import './collections-overview.styles.scss';
 
-import { selectCollections } from './../../redux/shop/shop.selectors'; 
+import { selectCollectionForPreview } from './../../redux/shop/shop.selectors'; 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 const CollectionsOverview = ({ collections}) => (
@@ -16,7 +16,7 @@ const CollectionsOverview = ({ collections}) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections : selectCollections
+    collections : selectCollectionForPreview
 })
 
 
